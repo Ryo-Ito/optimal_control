dde8d9326c196777f8c8337e2094180669ebf6c9
ryoito <r0735nj5058@gmail.com>
2017-01-25 17:30:30 +0900

    succeeded

--- run.py
+++ run.py
@@ -1,19 +1,24 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-N = 10  # number of steps
+N = 100  # number of steps
 M = 2  # dimensionality
 
 
 class Cost(object):
 
-    def __init__(self, y0, a, b):
+    def __init__(self, y0, a, b, c):
         self.y0 = y0
         self.a = a
         self.b = b
+        self.c = c
+
+    def reading_speed(self, x):
+        return self.y0 - self._sigmoid(x)
 
     def _sigmoid(self, x):
-        return np.divide(self.a, 1 + np.exp(self.b - x))
+        return np.divide(self.a, 1 + np.exp(self.c * (self.b - x)))
 
     def __call__(self, x):
         return np.sum(
@@ -26,7 +31,7 @@ class Cost(object):
         assert x.ndim == 1
         sig = self._sigmoid(x[1])
         deriv_x0 = 2 * (x[0] - self.y0 + sig)
-        deriv_x1 = 2 * (self.y0 - sig - x[0]) * sig * (sig - 1)
+        deriv_x1 = 2 * (self.y0 - sig - x[0]) * sig * (sig / self.a - 1) * self.c
         deriv = np.array([deriv_x0, deriv_x1])
         return deriv
 
@@ -80,18 +85,26 @@ class Model(object):
 
 
 def main():
+    cost_func = Cost(y0=10, a=3, b=500, c=0.01)
     model = Model(
-        x0=np.array([1., 0.]),
+        x0=np.array([10., 0.]),
         A=np.array([[1., 0.], [1., 1.]]),
         b=np.array([1., 0.]),
-        cost=Cost(y0=2, a=1, b=-5))
+        cost=cost_func)
     U = np.zeros(N)
     model.forward(U)
-    print(model.cost(model.X))
-    U = model.find_optimal_input(U, 10, learning_rate=1e-5)
-    print(U)
+    plt.plot(model.X[:, 0], c="b", label="uncontrolled app's speed")
+    reading_speed = cost_func.reading_speed(model.X[:, 1])
+    plt.plot(reading_speed, c="g", label="uncontrolled reader's speed")
+    U = model.find_optimal_input(U, 10000, learning_rate=1e-4)
     model.forward(U)
-    print(model.cost(model.X))
+    plt.plot(model.X[:, 0], c="b", linestyle="dashed", lw=2., label="controlled app's speed")
+    plt.plot(cost_func.reading_speed(model.X[:, 1]), c="g", linestyle="dotted", lw=5., label="controlled reader's speed")
+    plt.ylim(6, 11)
+    plt.xlabel("time (second)")
+    plt.ylabel("speed (word / second)")
+    plt.legend(loc="best")
+    plt.savefig("result.eps")
 
 
 if __name__ == '__main__':
